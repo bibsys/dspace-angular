75009ff65775cc3027591e4980de3b536f63eff4
MichaëlP <michael.pourbaix@uclouvain.be>
2024-02-02 15:10:48 +0100

    [ADD] feat(submission-button): Navbar submission button + check for authorization.
    New check for 'canCreateSubmission' feature with the button present in mydspace page that allows to create a submission.
    If the student has no pending submission he can submit.
    
    Also added a new button in the navbar to sumit which is also hcecking the feature to know if it can display.

---
+++ src/app/shared/new-submission-button/new-submission-button.component.ts
@@ -0,0 +1,9 @@
+import { MyDSpaceNewSubmissionDropdownComponent } from 'src/app/my-dspace-page/my-dspace-new-submission/my-dspace-new-submission-dropdown/my-dspace-new-submission-dropdown.component';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ds-new-submission-button',
+    templateUrl: './new-submission-button.component.html',
+    styleUrls: ['./new-submission-button.component.scss']
+})
+export class NewSubmissionButtonComponent extends MyDSpaceNewSubmissionDropdownComponent{}
