5f6b63cdeb53fd4d66b037ecb8dd795481c82204
MichaëlP <michael.pourbaix@uclouvain.be>
2024-02-06 10:34:50 +0100

    [ADD] feat(file-widget) Customizing the file widget section

---
+++ src/themes/uclouvain/app/submission/sections/upload/accessConditions/submission-section-upload-access-conditions.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { SubmissionSectionUploadAccessConditionsComponent as BaseComponent } from 'src/app/submission/sections/upload/accessConditions/submission-section-upload-access-conditions.component';
+
+@Component({
+    selector: 'ds-submission-section-upload-access-conditions',
+    templateUrl: './submission-section-upload-access-conditions.component.html'
+})
+export class SubmissionSectionUploadAccessConditionsComponent extends BaseComponent {}
---
+++ src/themes/uclouvain/app/submission/sections/upload/section-upload.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { SubmissionSectionUploadComponent as BaseComponent } from 'src/app/submission/sections/upload/section-upload.component';
+
+@Component({
+    selector: 'ds-submission-section-upload',
+    templateUrl: './section-upload.component.html'
+})
+export class SubmissionSectionUploadComponent extends BaseComponent {}
