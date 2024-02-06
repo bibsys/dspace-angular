952f486bb474c820d4f2bc947b4e04ed5950a3fb
MichaëlP <michael.pourbaix@uclouvain.be>
2024-02-06 11:36:23 +0100

    [UPD] feat(general): Global UI customization to fit DIAL.pr
    Global UI overhaul:
        - Removal of statistics on the home page and on the research page
        - Removal of 'Most viewed' home section
        - New color palette
        - Added logo and 'hero' section
        - Some bootstrap customization to get rid of the classic bootstrap feeling
        - Lot of changes in the i18n files to fit the DIAL.pr project
        - Changing badges vocab for elements in myDIAL
        - Customize the home search form
        - Customize facets design
        - Customize footer design
    
    Co-authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -574,7 +574,8 @@ export const environment: BuildConfig = {
   layout: {
     navbar: {
       // If true, show the "Community and Collections" link in the navbar; otherwise, show it in the admin sidebar
-      showCommunityCollection: true,
+      showCommunityCollection: false,
+      showStatistics: false,
     },
   },
   security: {
---
+++ src/themes/uclouvain/app/shared/search/search.component.ts
@@ -0,0 +1,50 @@
+import { AsyncPipe, NgIf, NgTemplateOutlet } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+import { ThemedSearchFormComponent } from '../../../../../app/shared/search-form/themed-search-form.component';
+import {
+  ItemExportModalLauncherComponent
+} from '../../../../../app/shared/search/item-export/item-export-modal-launcher/item-export-modal-launcher.component';
+import { SearchChartsComponent } from '../../../../../app/shared/search/search-charts/search-charts.component';
+import { SearchLabelsComponent } from '../../../../../app/shared/search/search-labels/search-labels.component';
+import {
+  ThemedSearchResultsComponent
+} from '../../../../../app/shared/search/search-results/themed-search-results.component';
+import {
+  ThemedSearchSidebarComponent
+} from '../../../../../app/shared/search/search-sidebar/themed-search-sidebar.component';
+import { SearchComponent as BaseComponent } from '../../../../../app/shared/search/search.component';
+import { pushInOut } from '../../../../../app/shared/animations/push';
+import { PageWithSidebarComponent } from '../../../../../app/shared/sidebar/page-with-sidebar.component';
+import { ViewModeSwitchComponent } from '../../../../../app/shared/view-mode-switch/view-mode-switch.component';
+
+@Component({
+  selector: 'ds-themed-search',
+  styleUrls: ['../../../../../app/shared/search/search.component.scss'],
+  templateUrl: './search.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [pushInOut],
+  imports: [
+    SearchChartsComponent,
+    NgIf,
+    NgTemplateOutlet,
+    AsyncPipe,
+    PageWithSidebarComponent,
+    ViewModeSwitchComponent,
+    TranslateModule,
+    ThemedSearchResultsComponent,
+    ThemedSearchSidebarComponent,
+    NgbTooltipModule,
+    ThemedSearchFormComponent,
+    ItemExportModalLauncherComponent,
+    SearchLabelsComponent
+  ],
+  standalone: true
+})
+
+/**
+ * This component renders a sidebar, a search input bar and the search results.
+ */
+export class SearchComponent extends BaseComponent {
+}
