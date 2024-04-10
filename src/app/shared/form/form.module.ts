bbdbe56ecde69df1e581454b45b3f650b333ed5f
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-04-10 15:32:26 +0200

    ADD feat(submission-widget): new submission widgets
    * Creates "HiddenField" widget for submission form : it allows to have a field into the form model but without
      any display on the screen (using "d-none" style isn't suffisant).
    * Derivates "DegreeSelect" from "EmptySelect" to have custom and specific behavior for Degree code management.
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>
    Co-Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- src/app/shared/form/form.module.ts
+++ src/app/shared/form/form.module.ts
@@ -67,9 +67,9 @@ import { NG_VALIDATORS } from '@angular/forms';
 import { DsYearPickerComponent } from './builder/ds-dynamic-form-ui/models/year-picker/year-picker.component';
 import { FormDynamicUpdateService } from './dynamic-fields/form.dynamic-update.service';
 import { FormDynamicUpdateRequestService } from './dynamic-fields/form.dynamic-update-request.service';
-import {
-  DsDynamicEmptySelectComponent
-} from './builder/ds-dynamic-form-ui/models/empty-select/dynamic-empty-select.component';
+import { DsDynamicEmptySelectComponent } from './builder/ds-dynamic-form-ui/models/empty-select/dynamic-empty-select.component';
+import { DsDynamicDegreeSelectComponent } from './builder/ds-dynamic-form-ui/models/degree-select/dynamic-degree-select.component';
+import { DsDynamicHiddenComponent } from './builder/ds-dynamic-form-ui/models/hidden/dynamic-hidden.component';
 
 const COMPONENTS = [
   CustomSwitchComponent,
@@ -93,9 +93,11 @@ const COMPONENTS = [
   DsDynamicRelationGroupComponent,
   DsDatePickerComponent,
   DsYearPickerComponent,
+  DsDynamicDegreeSelectComponent,
   DsDynamicEmptySelectComponent,
   DsDynamicFormGroupComponent,
   DsDynamicFormArrayComponent,
+  DsDynamicHiddenComponent,
   DsDatePickerInlineComponent,
   ExistingMetadataListElementComponent,
   ExistingRelationListElementComponent,
