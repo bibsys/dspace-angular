e717a627217ca1224f1fd67bbe3b4ddf0a9f9822
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-04-21 15:34:42 +0200

    [ADD] feat(submission-form): Custom upload file submission UI
    * Customizes upload file submission form component.
    * Adds translation for file access conditions.
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

---
+++ src/themes/uclouvain/app/submission/sections/upload/file/license/section-upload-file-license.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnChanges } from '@angular/core';
+import mapping from './mapping.json';
+import { isNotEmpty } from '../../../../../../../../app/shared/empty.util';
+import { NgIf } from '@angular/common';
+
+
+@Component({
+  selector: 'ds-submission-upload-section-license',
+  template: `<img *ngIf="imagePath" [src]="imagePath" [alt]="licenseUri" [height]="height"/>`,
+  imports: [NgIf],
+  standalone: true,
+})
+export class SubmissionSectionUploadFileLicenseComponent implements OnChanges {
+
+  @Input() licenseUri: string;
+  @Input() imageDirectoryPath: string = '/assets/uclouvain/images/cc-licences-badges';
+  @Input() height: number = 20;
+  imagePath: string;
+
+
+  ngOnChanges(changes: any) {
+    if (changes.hasOwnProperty('licenseUri')) {
+      const icon = mapping[this.extractLicenseKey()] || undefined;
+      if (icon !== undefined) {
+        this.imagePath = this.imageDirectoryPath + '/' + icon;
+      }
+    }
+  }
+
+  private extractLicenseKey(): string {
+    const urlSequences = new URL(this.licenseUri).pathname
+      .split('/')
+      .filter(e => isNotEmpty(e));
+    return (urlSequences.length > 0)
+      ? 'cc-' + urlSequences[1]
+      : undefined;
+  }
+}
