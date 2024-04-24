cea8a7c09226d1ba2f13106b1e551e1d5123ec28
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-04-24 09:10:11 +0200

    [UPD] feat(submission-form): Improvment for "dropdown" et "year" widget.
    * Dropdown widget
      - Adds possility to disable "trash button"
      - Adds option size limit to display the search field into dropdown
      - Creates new model for "masterthesis.session" field allowing default value depending on current date.
    * Year widget
      - Update model to allow default value depdending on current date.
      - Removes specific configuration from environment file.
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -100,12 +100,6 @@ export const environment: BuildConfig = {
     validatorMap: {
       required: 'required',
       regex: 'pattern'
-    },
-    fields: {
-      year: {
-        minYearDelta: 100,
-        maxYearDelta: 2
-      }
     }
   },
 
