5bc5edbf1a372ea6fea14122debf9ac0a840925f
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-05-06 17:21:04 +0200

    [UPD] feat(UI): Design UCLouvain theme
    Co-Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/themes/uclouvain/lazy-theme.module.ts
+++ src/themes/uclouvain/lazy-theme.module.ts
@@ -10,6 +10,12 @@ import { BadgesComponent } from './app/shared/object-collection/shared/badges/ba
 import { BrowseMostElementsComponent } from './app/browse-most-elements/browse-most-elements.component';
 import { ItemListPreviewComponent } from './app/shared/object-list/my-dspace-result-list-element/item-list-preview/item-list-preview.component';
 import { LoginPageComponent } from './app/login-page/login-page.component';
+import { MetadataRepresentationListComponent } from './app/item-page/simple/metadata-representation-list/metadata-representation-list.component';
+import { ObjectListComponent } from './app/shared/object-list/object-list.component'
+import { SearchResultsComponent } from './app/shared/search/search-results/search-results.component';
+import { SearchSidebarComponent } from './app/shared/search/search-sidebar/search-sidebar.component';
+import { SearchFiltersComponent } from './app/shared/search/search-filters/search-filters.component';
+import { SearchComponent } from './app/shared/search/search.component';
 
 import { CommonModule } from '@angular/common';
 import { AdminRegistriesModule } from '../../app/admin/admin-registries/admin-registries.module';
@@ -83,6 +89,12 @@ const DECLARATIONS = [
   AdminSidebarComponent,
   SubmissionSectionUploadFileComponent,
   SubmissionSectionUploadFileLicenseComponent,
+  MetadataRepresentationListComponent,
+  SearchResultsComponent,
+  ObjectListComponent,
+  SearchSidebarComponent,
+  SearchFiltersComponent,
+  SearchComponent
 ];
 
 @NgModule({
