5ba1ae0f392361e337ea6cfb503dd0f9005d025f
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-08-26 16:07:04 +0200

    [FIX] feat(general): Fix some problems for thesis
    * Adds missing translatable strings (and french translation).
    * Allow browse link on multiple metadata generic field displayed as list.
    * Removes API token fieldset on profile page
    * Allows disabling research profile deletion
    * Removes bulk import feature
    * Fix item accessStatus badge (using Item/accessStatus subresource); removes unused accessConditions specific service.
    * Allow search result sort only for search result page, not for browse results.
    
    Co-authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/app/profile-page/profile-page-researcher-form/profile-page-researcher-form.component.ts
+++ src/app/profile-page/profile-page-researcher-form/profile-page-researcher-form.component.ts
@@ -7,10 +7,11 @@ import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateService } from '@ngx-translate/core';
 import {
-  BehaviorSubject,
+  BehaviorSubject, combineLatest,
   Observable,
 } from 'rxjs';
 import {
+  first,
   map,
   mergeMap,
   switchMap,
@@ -34,6 +35,8 @@ import { NotificationsService } from '../../shared/notifications/notifications.s
 import { followLink } from '../../shared/utils/follow-link-config.model';
 import { ProfileClaimService } from '../profile-claim/profile-claim.service';
 import { ProfileClaimItemModalComponent } from '../profile-claim-item-modal/profile-claim-item-modal.component';
+import { RoleType } from '../../core/roles/role-types';
+import { RoleService } from '../../core/roles/role.service';
 
 @Component({
   selector: 'ds-profile-page-researcher-form',
@@ -77,7 +80,8 @@ export class ProfilePageResearcherFormComponent implements OnInit {
               protected notificationService: NotificationsService,
               protected authService: AuthService,
               protected router: Router,
-              protected modalService: NgbModal) {
+              protected modalService: NgbModal,
+              protected roleService: RoleService) {
 
   }
 
@@ -223,4 +227,14 @@ export class ProfilePageResearcherFormComponent implements OnInit {
     });
   }
 
+  public hasRoles(roles: RoleType | RoleType[]): Observable<boolean> {
+    const toValidate: RoleType[] = (Array.isArray(roles)) ? roles : [roles];
+    const checks: Observable<boolean>[] = toValidate.map((role) => this.roleService.checkRole(role));
+    return combineLatest(checks).pipe(
+      map((permissions: boolean[]) => permissions.includes(true)),
+      first()
+    );
+  }
+
+  protected readonly RoleType = RoleType;
 }
