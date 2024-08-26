88715612ae928dc5bc488877ae28da3d08fe8eec
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-08-26 16:07:04 +0200

    [FIX] feat(general): Fix some problems for thesis
    * Adds missing translatable strings (and french translation).
    * Allow browse link on multiple metadata generic field displayed as list.
    * Removes API token fieldset on profile page
    * Allows disabling research profile deletion
    * Removes bulk import feature
    * Fix item accessStatus badge (using Item/accessStatus subresource); removes unused accessConditions specific service.
    * Allow search result sort only for search result page, not for browse results.
    
    Co-authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -277,7 +277,7 @@ export const environment: BuildConfig = {
       undoTimeout: 10000 // 10 seconds
     },
     // Show the item access status label in items lists
-    showAccessStatuses: false,
+    showAccessStatuses: true,
     bitstream: {
       // Number of entries in the bitstream list in the item view page.
       // Rounded to the nearest size in the list of selectable sizes on the
--- src/themes/uclouvain/app/shared/log-in/log-in.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { LogInComponent as BaseComponent } from 'src/app/shared/log-in/log-in.component';
-
-@Component({
-    selector: 'ds-log-in',
-    templateUrl: './log-in.component.html',
-    styleUrls: ['../../../../../app/shared/log-in/log-in.component.scss'],
-})
-export class LogInComponent extends BaseComponent {}
