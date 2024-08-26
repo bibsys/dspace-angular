b6edb12134ddd5cc0da8d18f7c03b7891683bc3f
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-08-26 16:07:04 +0200

    [FIX] feat(general): Fix some problems for thesis
    * Adds missing translatable strings (and french translation).
    * Allow browse link on multiple metadata generic field displayed as list.
    * Removes API token fieldset on profile page
    * Allows disabling research profile deletion
    * Removes bulk import feature
    * Fix item accessStatus badge (using Item/accessStatus subresource); removes unused accessConditions specific service.
    * Allow search result sort only for search result page, not for browse results.
    
    Co-authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/themes/uclouvain/app/shared/object-list/object-list.component.ts
+++ src/themes/uclouvain/app/shared/object-list/object-list.component.ts
@@ -1,13 +1,5 @@
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ObjectListComponent as BaseComponent} from '../../../../../app/shared/object-list/object-list.component';
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
-import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
-import { SortDirection, SortOptions } from '../../../../../app/core/cache/models/sort-options.model';
-import { SearchConfig } from '../../../../../app/core/shared/search/search-filters/search-config.model';
-import { SearchConfigurationService } from '../../../../../app/core/shared/search/search-configuration.service';
-import { SelectableListService } from '../../../../../app/shared/object-list/selectable-list/selectable-list.service';
-import { PaginationService } from '../../../../../app/core/pagination/pagination.service';
-import { SEARCH_CONFIG_SERVICE } from 'src/app/my-dspace-page/my-dspace-configuration.service';
 import { PaginationComponent } from 'src/app/shared/pagination/pagination.component';
 import { SelectableListItemControlComponent } from 'src/app/shared/object-collection/shared/selectable-list-item-control/selectable-list-item-control.component';
 import { ImportableListItemControlComponent } from 'src/app/shared/object-collection/shared/importable-list-item-control/importable-list-item-control.component';
@@ -16,10 +8,10 @@ import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 import { BrowserOnlyPipe } from 'src/app/shared/utils/browser-only.pipe';
+import { SearchConfigurationService } from '../../../../../app/core/shared/search/search-configuration.service';
+import { SEARCH_CONFIG_SERVICE } from 'src/app/my-dspace-page/my-dspace-configuration.service';
 
-/** A component to display a list of objects and widget to navigate through them
- *    For UCLouvain theme, we should display the "sort by" widget at top the result list instead into the sidebar.
- */
+/** A component to display a list of objects and widget to navigate through them */
 @Component({
   selector: 'ds-themed-object-list',
   styleUrls: ['../../../../../app/shared/object-list/object-list.component.scss', './object-list.component.scss'],
@@ -45,57 +37,10 @@ import { BrowserOnlyPipe } from 'src/app/shared/utils/browser-only.pipe';
     }
   ]
 })
-export class ObjectListComponent extends BaseComponent implements OnInit, OnDestroy {
-
-  // COMPONENT ATTRIBUTES =====================================================
-  sortOptionsList$: BehaviorSubject<SortOptions[]> = new BehaviorSubject<SortOptions[]>([]);
-
-  protected readonly SortDirection = SortDirection;
-  private subs: Subscription[] = [];
-
-  // CONSTRUCTOR & HOOKS ======================================================
-  constructor(
-    protected paginationService: PaginationService,
-    protected selectionService: SelectableListService,
-    @Inject(SEARCH_CONFIG_SERVICE) public searchConfigService: SearchConfigurationService,
-  ){
-    super(selectionService);
-  }
+export class ObjectListComponent extends BaseComponent implements OnInit {
 
   /** OnInit hook */
   ngOnInit() {
     this.hasBorder = true;  // force border-bottom
-    const configuration$: Observable<string> = this.searchConfigService
-      .getCurrentConfiguration('')
-      .pipe(distinctUntilChanged());
-    const searchSortOptions$: Observable<SortOptions[]> = configuration$
-      .pipe(
-        switchMap((configuration: string) => this.searchConfigService.getConfigurationSearchConfig(configuration)),
-        map((searchConfig: SearchConfig) => this.searchConfigService.getConfigurationSortOptions(searchConfig)),
-        distinctUntilChanged()
-      );
-
-    this.subs.push(
-      searchSortOptions$.subscribe((options: SortOptions[]) => this.sortOptionsList$.next(options))
-    );
-  }
-
-  /** OnDestroy hook */
-  ngOnDestroy() {
-    this.subs.forEach(s => s.unsubscribe());
-  }
-
-  // COMPONENT FUNCTIONS ======================================================
-  /**
-   * Handler to manage sort result changes.
-   * @param event The triggered event.
-   */
-  reloadOrder(event: Event): void {
-    const values = (event.target as HTMLInputElement).value.split(',');
-    this.paginationService.updateRoute(this.searchConfigService.paginationID, {
-      sortField: values[0],
-      sortDirection: values[1] as SortDirection,
-      page: 1
-    });
   }
 }
