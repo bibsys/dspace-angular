9a158eeba16907b09da3826a3cd41f57b2889b53
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-09-25 19:00:30 +0200

    [UPD] feat(mydial): Fixing display stuffs on MyDIAL page.
    * Updates item snippet into MyDIAL page (fields, badges, additional information, ...)
    * Fixes "isController" function
    * Updates some translations
    * Removes "upload-file" widget into MyDIAL page
    * Removes export button
    
    Co-authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/themes/uclouvain/app/shared/object-list/my-dspace-result-list-element/item-list-preview/item-list-preview.component.ts
+++ src/themes/uclouvain/app/shared/object-list/my-dspace-result-list-element/item-list-preview/item-list-preview.component.ts
@@ -1,5 +1,5 @@
 import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { MetadataLinkViewComponent } from 'src/app/shared/metadata-link-view/metadata-link-view.component';
 import { ThemedBadgesComponent } from 'src/app/shared/object-collection/shared/badges/themed-badges.component';
@@ -11,6 +11,13 @@ import { AdditionalMetadataComponent } from 'src/app/shared/object-list/search-r
 import { TruncatablePartComponent } from 'src/app/shared/truncatable/truncatable-part/truncatable-part.component';
 import { TruncatableComponent } from 'src/app/shared/truncatable/truncatable.component';
 import { ThemedThumbnailComponent } from 'src/app/thumbnail/themed-thumbnail.component';
+import { AccessConditionObject } from '../../../../../../../app/core/submission/models/access-condition.model';
+import { isEmpty } from '../../../../../../../app/shared/empty.util';
+import { getFirstSucceededRemoteDataWithNotEmptyPayload } from '../../../../../../../app/core/shared/operators';
+import {
+  AccessStatusObject
+} from '../../../../../../../app/shared/object-collection/shared/badges/access-status-badge/access-status.model';
+import { AccessConditionsComponent } from '../../../access-conditions/access-condition.component';
 
 @Component({
     selector: 'ds-themed-item-list-preview',
@@ -30,7 +37,30 @@ import { ThemedThumbnailComponent } from 'src/app/thumbnail/themed-thumbnail.com
         NgFor,
         TranslateModule,
         AsyncPipe,
+        AccessConditionsComponent,
     ],
 	standalone: true,
 })
-export class ItemListPreviewComponent extends BaseComponent {}
+export class ItemListPreviewComponent extends BaseComponent implements OnInit {
+
+  accessCondition: AccessConditionObject;
+  authorMetadata = ['dc.contributor.author'];
+  promoterMetadata = ['dc.contributor.advisor'];
+  degreeCodes: string[];
+
+  ngOnInit() {
+    super.ngOnInit();
+    if (isEmpty(this.degreeCodes)) {
+      this.degreeCodes = this.item.allMetadataValues('masterthesis.degree.code');
+    }
+    if (this.item?.accessStatus) {
+      this.item.accessStatus
+        .pipe(getFirstSucceededRemoteDataWithNotEmptyPayload())
+        .subscribe((access: AccessStatusObject) => {
+          this.accessCondition = Object.assign(new AccessConditionObject(), {id: 0, name: access.status});
+        });
+    }
+  }
+
+  protected readonly isEmpty = isEmpty;
+}
