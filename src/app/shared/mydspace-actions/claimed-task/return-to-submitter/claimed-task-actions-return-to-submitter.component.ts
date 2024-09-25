ed5261e8223d932e422368847b4a5aadede86ffb
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2024-09-25 14:58:16 +0200

    [UPD] feat(workflow-actions) Send back to submission + removed reason + confirm boxes.
    - New button to send the item back to submissions state. When clicking it, the manager must request changes.
    - Removed reason requirement for both 'reject' and 'approve without diffusion'.
    - Added confirm box for 'reject', 'approve' and 'approve without diffusion'.
    
    @Authored-By: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ src/app/shared/mydspace-actions/claimed-task/return-to-submitter/claimed-task-actions-return-to-submitter.component.ts
@@ -0,0 +1,29 @@
+import { Component } from "@angular/core";
+import { ClaimedTaskActionsRejectComponent } from "../reject/claimed-task-actions-reject.component";
+import { NgbTooltipModule } from "@ng-bootstrap/ng-bootstrap";
+import { AsyncPipe, NgIf } from "@angular/common";
+import { TranslateModule } from "@ngx-translate/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+
+export const WORKFLOW_TASK_OPTION_RETURN_TO_SUBMITTER = 'submit_return_to_submitter';
+
+@Component({
+  selector: 'ds-claimed-task-actions-return-to-submitter',
+  templateUrl: './claimed-task-actions-return-to-submitter.component.html',
+  imports: [
+    NgbTooltipModule,
+    NgIf,
+    AsyncPipe,
+    TranslateModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  standalone: true,
+})
+/**
+ * Component to render the return to submitter action on a workflow task item.
+ */
+export class ClaimedTaskActionsReturnToSubmitterComponent extends ClaimedTaskActionsRejectComponent {
+  option = WORKFLOW_TASK_OPTION_RETURN_TO_SUBMITTER;
+}
