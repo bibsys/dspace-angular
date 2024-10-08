b829403b80672ebe7276c85a80c3ae320fee3254
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2024-10-08 13:58:23 +0200

    [UPD] feat(workflow): Display detail page as default + custom workflow messages
    * For any submission into MyDspace section, use the detail display page instead of technical display page.
    * Improve change-request container: Hide it if no change are requested.
    
    Co-Authored-by: Michael Pourbaix <michael.pourbaix@uclouvain.be>
    Co-Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/app/shared/mydspace-actions/claimed-task/approve_no_diffusion/claimed-task-actions-approve-no-diffusion.component.ts
+++ src/app/shared/mydspace-actions/claimed-task/approve_no_diffusion/claimed-task-actions-approve-no-diffusion.component.ts
@@ -5,6 +5,8 @@ import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { BtnDisabledDirective } from 'src/app/shared/btn-disabled.directive';
 import { ClaimedTaskActionsConfirmAbstractComponent } from '../abstract/claimed-task-actions-confirm-abstract.component';
+import { DSpaceObject } from 'src/app/core/shared/dspace-object.model';
+import { ClaimedApproveWithoutDiffusionTaskSearchResult } from 'src/app/shared/object-collection/shared/claimed-approve-without-diffusion-search-result.model';
 
 export const WORKFLOW_TASK_OPTION_APPROVE_NO_DIFFUSION = 'submit_approve_no_diffusion';
 
@@ -21,4 +23,11 @@ export const WORKFLOW_TASK_OPTION_APPROVE_NO_DIFFUSION = 'submit_approve_no_diff
  */
 export class ClaimedTaskActionsApproveNoDiffusionComponent extends ClaimedTaskActionsConfirmAbstractComponent {
   option = WORKFLOW_TASK_OPTION_APPROVE_NO_DIFFUSION;
+
+  convertReloadedObject(dso: DSpaceObject): DSpaceObject {
+    const reloadedObject = Object.assign(new ClaimedApproveWithoutDiffusionTaskSearchResult(), dso, {
+      indexableObject: dso
+    });
+    return reloadedObject;
+  }
 }
--- src/app/shared/mydspace-actions/pool-task/pool-task-actions.component.ts
+++ src/app/shared/mydspace-actions/pool-task/pool-task-actions.component.ts
@@ -33,11 +33,11 @@ import { ClaimedTaskDataService } from '../../../core/tasks/claimed-task-data.se
 import { PoolTask } from '../../../core/tasks/models/pool-task-object.model';
 import { ProcessTaskResponse } from '../../../core/tasks/models/process-task-response';
 import { PoolTaskDataService } from '../../../core/tasks/pool-task-data.service';
-import { getWorkflowItemViewRoute } from '../../../workflowitems-edit-page/workflowitems-edit-page-routing-paths';
 import { BtnDisabledDirective } from '../../btn-disabled.directive';
 import { NotificationsService } from '../../notifications/notifications.service';
 import { MyDSpaceReloadableActionsComponent } from '../mydspace-reloadable-actions';
 import { PdfAttestationActionComponent } from '../pdf-attestation-action/pdf-attestation-action.component';
+import { getItemPageRoute } from '../../../item-page/item-page-routing-paths';
 
 /**
  * This component represents mydspace actions related to PoolTask object.
@@ -72,6 +72,9 @@ export class PoolTaskActionsComponent extends MyDSpaceReloadableActionsComponent
   public itemUuid: string;
 
   subs = [];
+
+  protected readonly  getItemPageRoute = getItemPageRoute;
+
 
   /**
    * Initialize instance variables
@@ -131,12 +134,4 @@ export class PoolTaskActionsComponent extends MyDSpaceReloadableActionsComponent
   ngOnDestroy() {
     this.subs.forEach((sub) => sub.unsubscribe());
   }
-
-  /**
-   * Get the workflowitem view route.
-   */
-  getWorkflowItemViewRoute(workflowitem: WorkflowItem): string {
-    return getWorkflowItemViewRoute(workflowitem?.id);
-  }
-
 }
