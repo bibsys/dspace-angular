d1c578ed85e0c54e534ffab95398e30a9f272b40
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2024-10-08 13:58:23 +0200

    [UPD] feat(workflow): Display detail page as default + custom workflow messages
    * For any submission into MyDspace section, use the detail display page instead of technical display page.
    * Improve change-request container: Hide it if no change are requested.
    
    Co-Authored-by: Michael Pourbaix <michael.pourbaix@uclouvain.be>
    Co-Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/app/submission/sections/container/change-request/changeRequestSectionContainer.component.ts
+++ src/app/submission/sections/container/change-request/changeRequestSectionContainer.component.ts
@@ -6,10 +6,18 @@ import { AbstractSectionContainerComponent } from '../abstract-section-container
 @Component({
   selector: 'ds-submission-section-container-change-request',
   template: `
-    <div class="alert alert-warning" role="alert">
-        <h4 class="alert-heading">{{ 'submission.sections.change-request.header' | translate }}</h4>
-        <ng-container *ngComponentOutlet="getSectionContent(); injector: objectInjector"></ng-container>
-    </div>
+   <div dsSection #sectionRef="sectionRef"
+        [attr.id]="'section_' + sectionData.id"
+        [mandatory]="sectionData.mandatory"
+        [opened]="sectionData.opened"
+        [submissionId]="submissionId"
+        [sectionType]="sectionData.sectionType"
+        [sectionId]="sectionData.id">
+     <div class="alert alert-warning" role="alert" *ngIf="(sectionRef.isEnabled() | async)">
+       <h4 class="alert-heading">{{ 'submission.sections.change-request.header' | translate }}</h4>
+       <ng-container *ngComponentOutlet="getSectionContent(); injector: objectInjector"></ng-container>
+     </div>
+   </div>
   `,
 })
 @renderSectionContainerFor(SectionsType.ChangeRequest)
