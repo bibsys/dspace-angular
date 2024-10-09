22b10085816dd663a46668a3fa39fdbc1b703f8f
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2024-10-09 11:53:30 +0200

    [ADD] feat(item-badges): Badge for request change item in MyDIAL.mem

---
+++ src/themes/uclouvain/app/shared/object-collection/shared/badges/status-badge/status-badge.component.ts
@@ -0,0 +1,39 @@
+import { NgIf } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import { TranslateModule } from "@ngx-translate/core";
+import { DSpaceObjectDataService } from "src/app/core/data/dspace-object-data.service";
+import { hasValue } from "src/app/shared/empty.util";
+import { StatusBadgeComponent as BaseComponent } from "src/app/shared/object-collection/shared/badges/status-badge/status-badge.component";
+
+@Component({
+    selector: 'ds-themed-status-badge',
+    templateUrl: './status-badge.component.html',
+    imports: [
+        NgIf,
+        TranslateModule,
+    ],
+    standalone: true,
+})
+export class StatusBadgeComponent extends BaseComponent implements OnInit {
+    /**
+     * If a change was requested by a manager for the submitter.
+     */
+    isChangeRequested = false;
+    
+    constructor(
+        protected dSpaceObjectDataService: DSpaceObjectDataService
+    ) {
+        super();
+    }
+
+    ngOnInit(): void {
+        let objectAsAny = this.object as any;
+        if (hasValue(objectAsAny.indexableObject)) {
+            objectAsAny = objectAsAny.indexableObject;
+        }
+        const objectExists = hasValue(objectAsAny);
+        this.privateBadge = objectExists && hasValue(objectAsAny.isDiscoverable) && !objectAsAny.isDiscoverable;
+        this.withdrawnBadge = objectExists && hasValue(objectAsAny.isWithdrawn) && objectAsAny.isWithdrawn;
+        this.isChangeRequested = objectExists && hasValue(objectAsAny.isChangeRequested) && objectAsAny.isChangeRequested;
+    }
+}
