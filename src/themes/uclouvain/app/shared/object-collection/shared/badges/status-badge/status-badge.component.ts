7a17f9424dab2fc3c1d01b64e392035ed50f2325
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-10-17 16:32:35 +0200

    [FIX] feat(general): Multiple displays fixes
    * Fix "subscriptions" into user menu: Disable entry link if `subscription` context menu is disabled by backend config.
    
    Co-authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/themes/uclouvain/app/shared/object-collection/shared/badges/status-badge/status-badge.component.ts
+++ src/themes/uclouvain/app/shared/object-collection/shared/badges/status-badge/status-badge.component.ts
@@ -1,23 +1,17 @@
 import { Component, OnInit } from "@angular/core";
-import { DSpaceObjectDataService } from "src/app/core/data/dspace-object-data.service";
 import { hasValue } from "src/app/shared/empty.util";
 import { StatusBadgeComponent as BaseComponent } from "src/app/shared/object-collection/shared/badges/status-badge/status-badge.component";
 
 @Component({
     selector: 'ds-status-badge',
-    templateUrl: './status-badge.component.html'
+    templateUrl: './status-badge.component.html',
+    styleUrls: ['./status-badge.component.scss']
 })
 export class StatusBadgeComponent extends BaseComponent implements OnInit {
     /**
      * If a change was requested by a manager for the submitter.
      */
     isChangeRequested = false;
-    
-    constructor(
-        protected dSpaceObjectDataService: DSpaceObjectDataService
-    ) {
-        super();
-    }
 
     ngOnInit(): void {
         let objectAsAny = this.object as any;
