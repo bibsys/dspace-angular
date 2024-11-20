6431963920a898dc2804c281a6a3689db620ce0c
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-11-20 15:03:16 +0100

    [ADD] feat(form): Improve dynamic relation group fields
    * Better "add" button design
    * Better "no items" element behavior
    * Custom CSS around the field
    * Improve tags fields (keywords) design
    * Fixes behavior problem on institution affiliation field
    
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/app/shared/form/builder/ds-dynamic-form-ui/models/affiliations/institution/institution-affiliation-select.model.ts
+++ src/app/shared/form/builder/ds-dynamic-form-ui/models/affiliations/institution/institution-affiliation-select.model.ts
@@ -4,5 +4,6 @@ import { DsDynamicInputModel } from '../../ds-dynamic-input.model';
 export const DYNAMIC_FORM_CONTROL_TYPE_INSTITUTION_AFFILIATION_SELECT = 'INSTITUTION_AFFILIATION_SELECT';
 
 export class DynamicInstitutionAffiliationSelectModel extends DsDynamicInputModel {
+    readonly autoComplete = 'off';
     @serializable() readonly type: string = DYNAMIC_FORM_CONTROL_TYPE_INSTITUTION_AFFILIATION_SELECT;
 }
