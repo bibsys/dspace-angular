22a1ed3b426f55552c13b2e2005e2a0482bcba00
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-11-26 15:31:55 +0100

    [ADD] feat(form): Add custom inline-labeled-group field type
    * Customization on display for affiliations.
    * Customization on display for authors.
    * Fix checkbox display
    
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

---
+++ src/themes/uclouvain/app/shared/form/builder/models/authors/authors-inline-labeled-group-content.component.ts
@@ -0,0 +1,26 @@
+import { NgIf } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { inlineLabeledGroupContentComponent } from '../../../../../../../../app/shared/form/builder/ds-dynamic-form-ui/models/relation-inline-labeled-group/dynamic-relation-inline-labeled-group.decorator';
+import { ChipsItem } from '../../../../../../../../app/shared/form/chips/models/chips-item.model';
+
+@inlineLabeledGroupContentComponent('dc.contributor.author')
+@Component({
+  selector: 'ds-author-inline-labeled-group-content',
+  styleUrls: ['./authors-inline-labeled-group-content.component.scss'],
+  templateUrl: './authors-inline-labeled-group-content.component.html',
+  standalone: true,
+  imports: [NgIf]
+})
+export class AuthorInlineLabeledGroupContentComponent {
+
+  chip: ChipsItem = null;
+
+  constructor(@Inject('chip') chip: ChipsItem) {
+    this.chip = chip;
+  }
+
+  isAuthorityLinked() {
+    return this.chip.hasMetadata('dc.contributor.author') && this.chip.item['dc.contributor.author'].authority;
+  }
+
+}
