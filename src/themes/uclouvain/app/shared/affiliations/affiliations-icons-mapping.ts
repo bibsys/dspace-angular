2891026b00e60a1f620c756ae3f3727881b0fa68
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-11-26 15:31:55 +0100

    [ADD] feat(form): Add custom inline-labeled-group field type
    * Customization on display for affiliations.
    * Customization on display for authors.
    * Fix checkbox display
    
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

---
+++ src/themes/uclouvain/app/shared/affiliations/affiliations-icons-mapping.ts
@@ -0,0 +1,5 @@
+export const affiliationIcons: Map<string, string> = new Map([
+    ['uclouvain', 'assets/uclouvain/images/chips/uclouvain-logo-16x16.png'],
+    ['unamur', 'assets/uclouvain/images/chips/unamur-logo-16x16.png'],
+    ['usl-b', 'assets/uclouvain/images/chips/uslb-logo-16x16.png'],
+])
