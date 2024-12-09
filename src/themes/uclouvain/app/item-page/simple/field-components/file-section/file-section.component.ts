e485f626068edd733dd5b5fdf1b38f5d0c24a19a
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2024-12-09 13:46:39 +0100

    [FIX] feat(bitstream-download-url) Added missing followLink statement.
    A "followLink" statement was missing to embed "download_url" to the bundle request.
    This is required to see the direct download url on the item view for managers.
    
    Authored-By: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- src/themes/uclouvain/app/item-page/simple/field-components/file-section/file-section.component.ts
+++ src/themes/uclouvain/app/item-page/simple/field-components/file-section/file-section.component.ts
@@ -26,7 +26,8 @@ export class FileSectionComponent extends BaseComponent {
       true,
       true,
       followLink('format'),
-      followLink('access')
+      followLink('access'),
+      followLink('download_url')
     );
   }
 }
