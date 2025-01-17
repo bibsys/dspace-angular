df210797166644a0a657b4c7f147a5534cbf2a16
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2025-01-17 11:06:58 +0100

    [UPD] feat(year): New year component behavior
    Updated the behavior of the year component to be configurable through the backend.
    We can define 'settings' in the backend configuration that will be used to configured the field:
    - 'useCurrentYearAsDefault' is a boolean to set if we want a default value for the field (in which case we use the current year).
    - 'minYearDelta' is the delta to process the minimum value of the field (currentYear - minDelta).
    - 'maxYearDelta' is the delta to process the maximum value of the field (currentYear + maxDelta).
    - 'minYear', only active if no 'maxYearDelta', sets the minYear.
    - 'maxYear', only active if no 'minYearDelta', sets the maxYear.
    
    Authored-By: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- src/app/shared/form/builder/parsers/year-field-parser.ts
+++ src/app/shared/form/builder/parsers/year-field-parser.ts
@@ -1,6 +1,7 @@
 import { DynamicDsYearPickerModel, DynamicDsYearPickerModelConfig } from '../ds-dynamic-form-ui/models/year-picker/year-picker.model';
 import { FormFieldMetadataValueObject } from '../models/form-field-metadata-value.model';
 import { FieldParser } from './field-parser';
+import { getSetting } from './parser.utils';
 
 /**
  * Parser for the 'YearPickerComponent'.
@@ -11,6 +12,12 @@ export class YearFieldParser extends FieldParser {
         const inputYearModelConfig: DynamicDsYearPickerModelConfig = this.initModel(null, false, true);
         inputYearModelConfig.legend = this.configData.label;
         inputYearModelConfig.disabled = inputYearModelConfig.readOnly;
+        inputYearModelConfig.useCurrentYear = getSetting(this.configData, 'useCurrentYearAsDefault', Boolean) || false;
+        inputYearModelConfig.minYearDelta = getSetting(this.configData, 'minYearDelta', Number);
+        inputYearModelConfig.maxYearDelta = getSetting(this.configData, 'maxYearDelta', Number);
+        inputYearModelConfig.minYear = getSetting(this.configData, 'minYear', Number);
+        inputYearModelConfig.maxYear = getSetting(this.configData, 'maxYear', Number);
+
         this.setValues(inputYearModelConfig as any, fieldValue);
         return new DynamicDsYearPickerModel(inputYearModelConfig);
     }
