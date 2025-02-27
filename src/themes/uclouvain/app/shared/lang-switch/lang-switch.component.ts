52d8be4822c5612137e6d370e61925d05abb0fd4
Renaud Michotte <renaud.michotte@uclouvain.be>
2025-02-27 11:05:13 +0100

    [UPD] feat(view): Improve language selection menu
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

---
+++ src/themes/uclouvain/app/shared/lang-switch/lang-switch.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { LangSwitchComponent as BaseComponent } from '../../../../../app/shared/lang-switch/lang-switch.component';
+
+@Component({
+  selector: 'ds-lang-switch',
+  styleUrls: ['./lang-switch.component.scss', '../../../../../app/shared/lang-switch/lang-switch.component.scss'],
+  templateUrl: './lang-switch.component.html',
+})
+export class LangSwitchComponent extends BaseComponent {
+
+  currentLangCode(): string {
+    return this.activeLangs.find((MyLangConfig) => MyLangConfig.code === this.translate.currentLang).code;
+  }
+}
