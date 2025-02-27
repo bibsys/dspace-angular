7185e69f46a89e1cd3601c8df9d5ff8a826518d7
Renaud Michotte <renaud.michotte@uclouvain.be>
2025-02-27 11:05:13 +0100

    [UPD] feat(view): Improve language selection menu
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/themes/uclouvain/eager-theme.module.ts
+++ src/themes/uclouvain/eager-theme.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './app/navbar/navbar.component';
 import { FooterComponent } from './app/footer/footer.component';
 import { ItemSearchResultListElementComponent } from './app/shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component';
 import { StartsWithDateComponent } from './app/shared/starts-with/date/starts-with-date.component';
+import { LangSwitchComponent } from './app/shared/lang-switch/lang-switch.component';
 
 /**
  * Add components that use a custom decorator to ENTRY_COMPONENTS as well as DECLARATIONS.
@@ -18,10 +19,11 @@ const ENTRY_COMPONENTS = [
 
 const DECLARATIONS = [
   ...ENTRY_COMPONENTS,
+  FooterComponent,
   HomeNewsComponent,
   HeaderComponent,
+  LangSwitchComponent,
   NavbarComponent,
-  FooterComponent,
   StartsWithDateComponent,
 ];
 
