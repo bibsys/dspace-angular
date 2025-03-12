debe80b376e8b713e6121b161f73ff1ab651255b
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2025-03-12 09:54:13 +0100

    [UPD] feat(config): Config update for easier docker deploy.
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- src/config/default-app-config.ts
+++ src/config/default-app-config.ts
@@ -329,6 +329,8 @@ export class DefaultAppConfig implements AppConfig {
     // Minimum number of characters required before performing a lookup.
     minChars: 3,
     dynamicFields: [],
+    disclaimerSectionFor: [],
+    enableShortcutPanelFor: [],
   };
 
   // Default Language in which the UI will be rendered if the user's browser language is not an active language
