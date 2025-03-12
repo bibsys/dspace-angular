927ffdded5bb3398461a5fd3dfecfd4de67739b5
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2025-03-12 09:54:13 +0100

    [UPD] feat(all): Fixes from BIUL feedback
    * Better visualization of "browse" link into the submission form
    * Better visualization of invlid fields into the submission form
    * Adds and improves translations
    * Adds some data into manager snippet view
    
    Authored-by: Michaël Pourbaix <renaud.michotte@uclouvain.be>

--- src/themes/uclouvain/app/shared/object-list/my-dspace-result-list-element/item-list-preview/item-list-preview.component.ts
+++ src/themes/uclouvain/app/shared/object-list/my-dspace-result-list-element/item-list-preview/item-list-preview.component.ts
@@ -1,6 +1,6 @@
 import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MetadataLinkViewComponent } from 'src/app/shared/metadata-link-view/metadata-link-view.component';
 import { ThemedBadgesComponent } from 'src/app/shared/object-collection/shared/badges/themed-badges.component';
 import { ItemCollectionComponent } from 'src/app/shared/object-collection/shared/mydspace-item-collection/item-collection.component';
@@ -11,6 +11,7 @@ import { AdditionalMetadataComponent } from 'src/app/shared/object-list/search-r
 import { TruncatablePartComponent } from 'src/app/shared/truncatable/truncatable-part/truncatable-part.component';
 import { TruncatableComponent } from 'src/app/shared/truncatable/truncatable.component';
 import { ThemedThumbnailComponent } from 'src/app/thumbnail/themed-thumbnail.component';
+import { DSONameService } from '../../../../../../../app/core/breadcrumbs/dso-name.service';
 import { AccessConditionObject } from '../../../../../../../app/core/submission/models/access-condition.model';
 import { isEmpty } from '../../../../../../../app/shared/empty.util';
 import { getFirstSucceededRemoteDataWithNotEmptyPayload } from '../../../../../../../app/core/shared/operators';
@@ -21,6 +22,8 @@ import { AccessConditionsComponent } from '../../../access-conditions/access-con
 import {
   PLACEHOLDER_PARENT_METADATA
 } from '../../../../../../../app/shared/form/builder/ds-dynamic-form-ui/ds-dynamic-form-constants';
+import { APP_CONFIG, AppConfig } from '../../../../../../../config/app-config.interface';
+import { TruncatableService } from 'src/app/shared/truncatable/truncatable.service';
 
 @Component({
     selector: 'ds-themed-item-list-preview',
@@ -51,6 +54,15 @@ export class ItemListPreviewComponent extends BaseComponent implements OnInit {
   promoterMetadata = ['dc.contributor.advisor'];
   degreeCodes: string[];
 
+  constructor(
+    @Inject(APP_CONFIG) protected appConfig: AppConfig,
+    public dsoNameService: DSONameService,
+    public truncateService: TruncatableService,
+    protected translateService: TranslateService,
+  ) {
+    super(appConfig, dsoNameService, truncateService);
+  }
+
   ngOnInit() {
     super.ngOnInit();
     if (isEmpty(this.degreeCodes)) {
@@ -68,4 +80,17 @@ export class ItemListPreviewComponent extends BaseComponent implements OnInit {
   }
 
   protected readonly isEmpty = isEmpty;
+
+  getDefenseDate() {
+    const parts: Array<String> = [];
+    if (this.item.hasMetadata("masterthesis.session")) {
+      parts.push(this.translateService.instant("session." + this.item.firstMetadataValue("masterthesis.session").toLowerCase()));
+    }
+    parts.push(this.item.hasMetadata("dc.date.issued")
+      ? this.item.firstMetadataValue("dc.date.issued")
+      : this.translateService.instant("mydspace.results.no-date")
+    );
+    return parts.join(" ");
+  }
+
 }
