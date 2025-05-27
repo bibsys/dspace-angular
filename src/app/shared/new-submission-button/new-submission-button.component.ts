9a2e6686c0f4e6d30a41126bffd47f85eb3b23f9
Renaud Michotte <renaud.michotte@uclouvain.be>
2025-05-27 14:00:29 +0200

    [FIX] feat(submit): Fix quick submit button
    Use the `CanSubmit` feature instead of `CanCreateSubmission`.
    
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/app/shared/new-submission-button/new-submission-button.component.ts
+++ src/app/shared/new-submission-button/new-submission-button.component.ts
@@ -1,5 +1,7 @@
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { MyDSpaceNewSubmissionDropdownComponent } from 'src/app/my-dspace-page/my-dspace-new-submission/my-dspace-new-submission-dropdown/my-dspace-new-submission-dropdown.component';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
@@ -11,4 +13,13 @@ import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
     imports: [NgIf, TranslateModule, AsyncPipe, NgbTooltipModule],
     standalone: true,
 })
-export class NewSubmissionButtonComponent extends MyDSpaceNewSubmissionDropdownComponent{}
+export class NewSubmissionButtonComponent extends MyDSpaceNewSubmissionDropdownComponent implements OnInit {
+
+  canSubmit$: Observable<boolean>;
+
+  ngOnInit() {
+    super.ngOnInit();
+    this.canSubmit$ = combineLatest([this.initialized$, this.authorized$])
+      .pipe(map(([initialized, authorized]) => initialized && authorized));
+  }
+}
