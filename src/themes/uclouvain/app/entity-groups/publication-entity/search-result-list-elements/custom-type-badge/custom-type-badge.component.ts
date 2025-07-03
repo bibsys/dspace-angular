72bc830c6fff8c0f10f47353e9d34e318cf6a6f0
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2025-07-03 13:16:13 +0200

    [UPD] feat(search-result) New display for search results.
    Each search results now displays in a component containing:
    
    - The type of the item, if 'publication' then we also display the type of publication.
    - The access-type of the item,
    - The title of the item,
    - The list of authors,
    - A section for the citation that will be implemented in the future.
    - Removed the scope selector in the search bar.
    
    Also fixed: redirect to item page instead of metadata view for workflow and workspace items.
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ src/themes/uclouvain/app/entity-groups/publication-entity/search-result-list-elements/custom-type-badge/custom-type-badge.component.ts
@@ -0,0 +1,82 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from 'src/app/core/shared/item.model';
+import { hasValue, isNotEmpty } from 'src/app/shared/empty.util';
+import { PUBLICATION_TYPES_MAPPING } from '../../type-label-mapping';
+import { DSpaceObject } from 'src/app/core/shared/dspace-object.model';
+import { getResourceTypeValueFor } from 'src/app/core/cache/object-cache.reducer';
+import { NgClass, NgIf } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+    selector: 'ds-custom-type-badge',
+    template: `<span *ngIf='entityType || publicationType' class='badge text-muted font-weight-bold py-1 px-2' [ngClass]=[customStyle]>
+        <ng-container *ngIf='isNotEmpty(entityType)'> {{ entityType | translate }} </ng-container>
+        <ng-container *ngIf='isNotEmpty(publicationType)'>
+            <ng-container *ngIf='isNotEmpty(entityType)'> / </ng-container>
+            {{ parsePublicationType(publicationType) | translate }}
+        </ng-container>
+    </span>`,
+    styles: ['span {font-size: 0.8rem; line-height: 1.5; box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 0px 1px;}'],
+    standalone: true,
+    imports: [
+        NgIf,
+        TranslateModule,
+        NgClass,
+    ]
+})
+export class CustomTypeBadgeComponent implements OnInit {
+    @Input() object: DSpaceObject;
+    @Input() displayEntityType: boolean = true;
+    @Input() customStyle = '';
+
+    protected entityType: string = undefined;
+    protected publicationType: string = undefined;
+
+    protected readonly isNotEmpty = isNotEmpty;
+
+    ngOnInit(): void {
+        if (isNotEmpty(this.object)) {
+            (this.object instanceof Item) ? this.typeFromItem(this.object): this.typeFromRenderedType(this.object);
+        }
+    }
+
+    /**
+     * Retrieve the types for an item.
+     * @param object The item to extract the type for.
+     */
+    typeFromItem(item: Item) {
+        let entityType = item.entityType.toLowerCase();
+        this.entityType = this.displayEntityType ? (entityType + '.listelement.badge'): undefined;
+        if (entityType === 'publication') {
+            this.publicationType = this.object.firstMetadataValue('dc.type.maintype');
+        }
+    }
+
+    /**
+     * Retrieve the literal string for a DSpace object.
+     * @param object The object to extract the type for.
+     */
+    typeFromRenderedType(object: DSpaceObject): void {
+        const renderTypes = object.getRenderTypes();
+        if (isNotEmpty(renderTypes.length)) {
+            const renderType = renderTypes[0];
+            if (renderType instanceof Function) {
+                const resourceTypeValue = getResourceTypeValueFor(object.type);
+                this.entityType = (hasValue(resourceTypeValue))
+                    ? `${resourceTypeValue.toLowerCase()}.listelement.badge`
+                    : `${renderType.name.toLowerCase()}.listelement.badge`;
+            } else {
+                    this.entityType = `${renderType.toLowerCase()}.listelement.badge`;
+            }
+        }
+        this.entityType = 'item.entity-type.unknown';
+    }
+
+    // Used to translate a publication type to a label that can be displayed.
+    parsePublicationType(type: string): string {
+        if (type.includes('text::')) {
+            return PUBLICATION_TYPES_MAPPING[type] ?? type.split('text::')[1];
+        }
+        return type;
+    }
+}
