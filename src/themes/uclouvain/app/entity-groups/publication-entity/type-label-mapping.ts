8be7c3764672b1abd68b81340f8388c83d1afefd
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2025-08-04 14:00:11 +0200

    [UPD] feat(authors-list): Retrieve auhtor role label
    Use the correct vocabulary to retrieve the author label and display it.
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- src/themes/uclouvain/app/entity-groups/publication-entity/type-label-mapping.ts
+++ src/themes/uclouvain/app/entity-groups/publication-entity/type-label-mapping.ts
@@ -13,3 +13,13 @@ export const PUBLICATION_TYPES_MAPPING: {[key: string]: string} = {
     "text::report": "publication.type.report.heading",
     "text::patent": "publication.type.patent.heading",
 }
+
+export const PUBLICATION_ROLES_VOCABULARIES_MAPPING: {[key: string]: string} = {
+    "text::journal-article": "publication_roles_journal_article",
+    "text::book": "publication_roles_book",
+    "text::book-part": "publication_roles_book_chapter",
+    "text::conference-speech": "publication_roles_conference_speech",
+    "text::working-paper": "publication_roles_working_paper",
+    "text::report": "publication_roles_report",
+    "text::patent": "publication_roles_patent",
+}
