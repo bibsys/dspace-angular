fc6b78151cd123b28bff2f0224311dca7e065c98
Renaud Michotte <renaud.michotte@uclouvain.be>
2025-11-05 13:54:57 +0100

    [UPD] feat(item-page): Better display for "et.al." metadata
    Authored-ny: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/themes/uclouvain/app/entity-groups/publication-entity/specific-field/author-format-display.component.ts
+++ src/themes/uclouvain/app/entity-groups/publication-entity/specific-field/author-format-display.component.ts
@@ -2,8 +2,6 @@ import { NgForOf, NgIf } from "@angular/common";
 import { Component, Input, OnInit } from "@angular/core";
 import { Item } from "src/app/core/shared/item.model";
 import { ItemLinkViewComponent } from "../../../shared/item-link-view/item-link-view.component";
-import { NgbTooltipModule } from "@ng-bootstrap/ng-bootstrap";
-import { TranslateModule } from "@ngx-translate/core";
 
 /**
  * Component to render the author list for a given item using the following rules:
@@ -18,21 +16,15 @@ import { TranslateModule } from "@ngx-translate/core";
     template: `<div class="d-flex align-items-center">
         <ng-container *ngFor="let author of authorList; index as i; let last=last;">
             <ds-item-link-view [metadataValue]="author"/>
-            <span *ngIf="!last" class="mr-2"> ; </span>
+            <span *ngIf="!last" class="mx-1">;</span>
         </ng-container>
-        <span *ngIf="hasEtal"
-              class="ml-1 font-italic text-muted"
-              ngbTooltip="{{ 'search.result.publication.authors.etal.tooltip' | translate }}">et.al.</span>
+        <ng-container *ngIf="hasEtal">
+            <span class="mx-1">;</span>
+            <span>et.al.</span>
+        </ng-container>
     </div>`,
-    styles: ['.orcid-icon { height: 1.1rem; }'],
     standalone: true,
-    imports: [
-        NgForOf,
-        ItemLinkViewComponent,
-        NgIf,
-        NgbTooltipModule,
-        TranslateModule,
-    ],
+    imports: [NgForOf, ItemLinkViewComponent, NgIf],
 })
 export class AuthorFormatDisplayComponent implements OnInit {
     @Input() item: Item;
@@ -44,11 +36,10 @@ export class AuthorFormatDisplayComponent implements OnInit {
     ngOnInit(): void {
         let authors = this.item.allMetadata(this.authorField);
         if (authors.length > 5) {
-            let finalAuthor = authors[authors.length - 1];
-            authors = authors.slice(0, 4);
-            authors.push(finalAuthor);
-            this.hasEtal = true;
+          authors = [...authors.slice(0, 4), authors[authors.length - 1]];
+          this.hasEtal = true;
         }
+        this.hasEtal = this.hasEtal || this.item.firstMetadataValue("dc.contributor.etal") === 'true';
         this.authorList = authors;
     }
 }
