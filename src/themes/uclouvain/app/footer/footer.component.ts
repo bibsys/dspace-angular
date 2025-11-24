4267dedc7d137e7223a2232539f374c0bdc13d2d
Renaud Michotte <renaud.michotte@uclouvain.be>
2025-11-24 14:01:21 +0100

    [FIX] feat(general): Add project sha-1 into footer
    Also fix shortcut setting overrided by environment variable
    
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/themes/uclouvain/app/footer/footer.component.ts
+++ src/themes/uclouvain/app/footer/footer.component.ts
@@ -1,8 +1,20 @@
 import { AsyncPipe, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Inject, Optional } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { NotifyInfoService } from '../../../../app/core/coar-notify/notify-info/notify-info.service';
+import { ConfigurationDataService } from '../../../../app/core/data/configuration-data.service';
+import { AuthorizationDataService } from '../../../../app/core/data/feature-authorization/authorization-data.service';
+import { SiteDataService } from '../../../../app/core/data/site-data.service';
+import { LocaleService } from '../../../../app/core/locale/locale.service';
+import { RoleService } from '../../../../app/core/roles/role.service';
+import { getFirstSucceededRemoteDataWithNotEmptyPayload } from '../../../../app/core/shared/operators';
 import { FooterComponent as BaseComponent } from '../../../../app/footer/footer.component';
+import { KlaroService } from '../../../../app/shared/cookies/klaro.service';
+import { APP_CONFIG, AppConfig } from '../../../../config/app-config.interface';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'ds-themed-footer',
@@ -16,4 +28,48 @@ import { FooterComponent as BaseComponent } from '../../../../app/footer/footer.
     AsyncPipe
   ]
 })
-export class FooterComponent extends BaseComponent {}
+export class FooterComponent extends BaseComponent {
+
+  isAdmin: boolean = false;
+  backendVersion: Observable<string>;
+  frontendVersion: Observable<string>;
+
+  constructor(
+    @Optional() public cookies: KlaroService,
+    protected authorizationService: AuthorizationDataService,
+    protected notifyInfoService: NotifyInfoService,
+    protected locale: LocaleService,
+    protected siteService: SiteDataService,
+    @Inject(APP_CONFIG) protected appConfig: AppConfig,
+    private configService: ConfigurationDataService,
+    private roleService: RoleService
+  ) {
+    super(cookies, authorizationService, notifyInfoService, locale, siteService, appConfig);
+  }
+
+  ngOnInit() {
+    super.ngOnInit();
+    this.initAppVersions();
+  }
+
+  /**
+   * Initialize application version (backend & frontend).
+   * This allows admin to visualize SHA-1 of deployed commits.
+   * @private
+   */
+  private initAppVersions() {
+    this.roleService.isAdmin().subscribe(
+      isAdmin => {
+        this.isAdmin = isAdmin;
+        if (isAdmin) {
+          this.backendVersion = this.configService
+            .findByPropertyName("uclouvain.project.version")
+            .pipe(
+              getFirstSucceededRemoteDataWithNotEmptyPayload(),
+              map(versions => versions.values[0])
+            );
+          this.frontendVersion = of(environment?.ui?.releaseVersion);
+        }
+      });
+  }
+}
