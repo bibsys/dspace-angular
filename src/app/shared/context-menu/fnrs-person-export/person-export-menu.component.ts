1fea05cca091e2e1a0e7ce46706a6a3872876db7
Renaud Michotte <renaud.michotte@uclouvain.be>
2026-03-06 11:13:15 +0100

    [UPD] feat(export): Update author bibliography export menu
    * Allow to filter on start/end year
    * Allow to include poster
    * Manager secured called (brearer/cookie) because backend API are now protected
    
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/app/shared/context-menu/fnrs-person-export/person-export-menu.component.ts
+++ src/app/shared/context-menu/fnrs-person-export/person-export-menu.component.ts
@@ -1,8 +1,10 @@
-import { Component, Inject, OnInit, TemplateRef, ViewChild } from "@angular/core";
-import { tap } from 'rxjs/operators';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Component, Inject, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AuthorizationDataService } from '../../../core/data/feature-authorization/authorization-data.service';
 import { FeatureID } from '../../../core/data/feature-authorization/feature-id';
 import { Item } from '../../../core/shared/item.model';
+import { NotificationsService } from '../../notifications/notifications.service';
 import { ContextMenuEntryComponent } from "../context-menu-entry.component";
 import { DSpaceObject } from "src/app/core/shared/dspace-object.model";
 import { DSpaceObjectType } from "src/app/core/shared/dspace-object-type.model";
@@ -12,75 +14,55 @@ import { TranslateModule } from "@ngx-translate/core";
 import { AsyncPipe, NgFor, NgIf } from "@angular/common";
 import { combineLatest, map, Observable, of } from "rxjs";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
-import { isEmpty, isNotEmpty } from "../../empty.util";
+import { isNotEmpty } from "../../empty.util";
 
 /**
  * Component to export a user profile bibliography.
  * 
- * @author Michaël Pourbaix <michael.pourbaix@uclouvain.be> 
+ * @author Michaël Pourbaix <michael.pourbaix@uclouvain.be>
+ * @author Renaud Michotte <renaud.michotte@uclouvain.be>
  */
 @Component({
   selector: 'ds-person-export-menu',
-  template: `
-    <button *ngIf="isAuthorized$ | async" class="btn btn-primary" (click)="openModal(exportModal)">
-      <span>{{ 'context-menu.actions.person-export' | translate }}</span>
-    </button>
-    <ng-template #exportModal let-c="close" let-d="dismiss">
-      <div class="modal-header">
-        <h4 class="modal-title">{{'context-menu.actions.person-export.modal.title' | translate}}</h4>
-        <button type="button"
-                class="close"
-                aria-label="Close"
-                (click)="d('Cross click')">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body d-flex justify-content-center">
-        <ng-container *ngFor="let export of availableExports">
-          <a class="export-box d-flex flex-column align-items-center justify-content-between m-3 p-3 border border-primary rounded"
-              [href]="export.exportPath"
-              target="_blank">
-            <img [src]="export.imagePath"/>
-            <span class="font-weight-bold">{{ "context-menu.actions.person-export.modal.export." + export.name | translate }}</span>
-          </a>
-        </ng-container>
-      </div>
-    </ng-template>
-  `,
-  styleUrl: './person-export-menu.component.scss',
+  templateUrl: './person-export-menu.component.html',
   standalone: true,
   imports: [
     TranslateModule,
     NgIf,
     AsyncPipe,
     NgFor,
+    ReactiveFormsModule
   ]
 })
 export class PersonExportMenuComponent extends ContextMenuEntryComponent implements OnInit {
-  protected readonly exportUrl = environment.rest.baseUrl + '/api/uclouvain/export';
-  protected availableExports: any[] = [];
+
+  protected readonly restBaseUrl = environment.rest.baseUrl + '/api/uclouvain/export';
   protected isAuthorized$: Observable<boolean> = of(false);
+  protected exportForm: FormGroup;
+  protected styles = [
+    { id: 'fnrs', label: 'FNRS', logo: 'assets/uclouvain/images/export/FNRS_logo.png' },
+    { id: 'fwb', label: 'FWB', logo: 'assets/uclouvain/images/export/FWB_logo.png' }
+  ];
 
   constructor(
     @Inject('contextMenuObjectProvider') protected injectedContextMenuObject: DSpaceObject,
     @Inject('contextMenuObjectTypeProvider') protected injectedContextMenuObjectType: DSpaceObjectType,
     protected authorizationService: AuthorizationDataService,
     protected modalService: NgbModal,
+    private formBuilder: FormBuilder,
+    private httpClient: HttpClient,
+    private notificationsService: NotificationsService,
   ) {
     super(injectedContextMenuObject, injectedContextMenuObjectType, ContextMenuEntryType.ExportItem);
   }
 
   ngOnInit(): void {
-    const profileUUID = this.contextMenuObject?.id;
+    this.initForm();
+
     this.isAuthorized$ = combineLatest([
       this.isObjectValid(),
       this.authorizationService.isAuthorized(FeatureID.CanExportPersonBibliography, this.contextMenuObject.self)
     ]).pipe(
-      tap(([valid, ignored]) => {
-        if (valid) {
-          this.availableExports = this.initAvailableExports(profileUUID);
-        }
-      }),
       map(([valid, authorized]) => valid && authorized)
     );
   }
@@ -89,17 +71,56 @@ export class PersonExportMenuComponent extends ContextMenuEntryComponent impleme
     this.modalService.open(content, { size: 'lg' })
   }
 
-  private initAvailableExports(profileUUID: string): any[] {
-    const baseUrl = `${this.exportUrl}/`;
-    const params = `?authorUUID=${profileUUID}`;
-    return [
-      { name: 'fnrs-export', image: 'FNRS_logo.png', path: 'fnrs' },
-      { name: 'fwb-export', image: 'FWB_logo.png', path: 'fwb' }
-    ].map(exp => ({
-      name: exp.name,
-      imagePath: `assets/uclouvain/images/export/${exp.image}`,
-      exportPath: baseUrl + exp.path + params
-    }));
+  private initForm() {
+    this.exportForm = this.formBuilder.group({
+      style: ['fnrs'], // default value
+      startYear: [null],
+      endYear: [null],
+      includePoster: [false]
+    });
+  }
+
+
+  onSubmit() {
+    const values = this.exportForm.value;
+    const profileUUID = this.contextMenuObject.id;
+
+    // Build baseURL : /api/uclouvain/export/{style}?uuid={uuid}
+    let url = `${this.restBaseUrl}/${values.style}?uuid=${profileUUID}`;
+
+    // Add additional parameters if needed
+    if (values.startYear) url += `&startYear=${values.startYear}`;
+    if (values.endYear) url += `&endYear=${values.endYear}`;
+    if (values.includePoster) url += `&includePoster=true`;
+
+    this.httpClient.get(url, {
+      responseType: 'blob',
+      observe: 'response'
+    }).subscribe({
+      next: (response: HttpResponse<Blob>) => {
+        this.downloadFile(response, values.style);
+        this.notificationsService.success(null, 'export.modal.process.success');
+      },
+      error: (err) => {
+        this.notificationsService.error(null, 'export.modal.process.error' + " : " + err);
+    }});
+  }
+
+  private downloadFile(response: HttpResponse<Blob>, style: string) {
+    const contentDisposition = response.headers.get('Content-Disposition');
+    const fileName = contentDisposition
+      ? contentDisposition.split('filename=')[1]?.replace(/"/g, '')
+      : `export-${style}.pdf`;
+
+    const blob = response.body;
+    if (blob) {
+      const downloadURL = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = downloadURL;
+      link.download = fileName;
+      link.click();
+      window.URL.revokeObjectURL(downloadURL);
+    }
   }
 
   private isObjectValid(): Observable<boolean> {
