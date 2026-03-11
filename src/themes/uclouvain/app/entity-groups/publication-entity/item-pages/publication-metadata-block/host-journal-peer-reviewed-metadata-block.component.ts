cc1c3fe719643535482acfd70dffb984ba26b763
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2026-03-11 09:13:00 +0100

    [Fix] feat(bug-fix): Fixing misc UI problems.
    - Fixed infinite loading dashboard when impersonating someone.
    - Fixed search result facet button delete action not working.
    - Position the filter rest button as sticky at the bottom of the page.
    - Fixed a bug when clicking 'reset filters' button when no filters are applied.
    - Fixed UCLouvain logo clipping with DIAL.mem logo.
    - Fixed journal metadata block not displaying for conference speech.
    - Fixed order of metadata blocks to be: 'journal info', 'publisher info' and then 'journal peer-reviewed'.
    - Added missing traductions.
    - Added a back button for the stats page.
    - Fixed a bug where clicking on the 'back' button when editing a thesis returned a 404.
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ src/themes/uclouvain/app/entity-groups/publication-entity/item-pages/publication-metadata-block/host-journal-peer-reviewed-metadata-block.component.ts
@@ -0,0 +1,31 @@
+import { Component } from "@angular/core";
+import { AbstractMetadataBlockComponent } from "./abstract-metadata-block.component";
+import { listableMetadataBlockComponent } from "../listable-metadata-block.decorator";
+import { ViewMode } from "src/app/core/shared/view-mode.model";
+import { Context } from "src/app/core/shared/context.model";
+import { NgIf } from "@angular/common";
+import { TranslateModule } from "@ngx-translate/core";
+
+@listableMetadataBlockComponent('text::journal-article', ViewMode.StandalonePage, Context.Any, '*', 32)
+@listableMetadataBlockComponent('text::conference-speech', ViewMode.StandalonePage, Context.Any, '*', 32)
+@Component({
+    template: `
+      <ng-container *ngIf="item.firstMetadataValue('publication.serial.peerReviewed')">
+        <dt>{{ 'journal.listelement.peerreviewed' | translate }}</dt>
+        <dd>{{ 'journal.listelement.peerreviewed.true' | translate }}</dd>
+      </ng-container>
+  `,
+  styles: `
+    .journal-metadata {
+        display: block;
+        color: var(--bs-secondary);
+        font-style: italic;
+    }
+  `,
+  standalone: true,
+  imports: [
+    NgIf,
+    TranslateModule,
+  ]
+})
+export class HostJournalPeerReviewedBlockComponent extends AbstractMetadataBlockComponent {}
