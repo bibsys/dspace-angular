915b3e2f880eb084e0368fc3bdbcd56f3080d6f6
Renaud Michotte <renaud.michotte@uclouvain.be>
2026-03-16 13:48:56 +0100

    [ADD] feat(general): Allow "delegator" to view submitter data into item detail page
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/app/core/roles/role-types.ts
+++ src/app/core/roles/role-types.ts
@@ -1,6 +1,8 @@
 export enum RoleType {
   Submitter = 'submitter',
   Controller = 'controller',
+  Delegator = 'delegator',
+  Manager = Controller,
   JournalManager = 'journal-manager',
   Admin = 'admin'
 }
