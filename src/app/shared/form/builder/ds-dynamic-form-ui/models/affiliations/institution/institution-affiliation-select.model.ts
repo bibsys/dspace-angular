0adb45c2d6ea3c430bc052d9c10a5c0006f34de6
Renaud Michotte <renaud.michotte@uclouvain.be>
2026-03-26 16:50:54 +0100

    [UPD] feat(form): Improve Affiliation institution select input
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- src/app/shared/form/builder/ds-dynamic-form-ui/models/affiliations/institution/institution-affiliation-select.model.ts
+++ src/app/shared/form/builder/ds-dynamic-form-ui/models/affiliations/institution/institution-affiliation-select.model.ts
@@ -1,9 +1,19 @@
-import { serializable } from '@ng-dynamic-forms/core';
-import { DsDynamicInputModel } from '../../ds-dynamic-input.model';
+import { DynamicFormControlLayout, serializable } from '@ng-dynamic-forms/core';
+import { DsDynamicInputModel, DsDynamicInputModelConfig } from '../../ds-dynamic-input.model';
 
 export const DYNAMIC_FORM_CONTROL_TYPE_INSTITUTION_AFFILIATION_SELECT = 'INSTITUTION_AFFILIATION_SELECT';
 
+export interface DsDynamicInstitutionAffiliationSelectModelConfig extends DsDynamicInputModelConfig {
+  excludedValues: Set<string>;
+}
+
 export class DynamicInstitutionAffiliationSelectModel extends DsDynamicInputModel {
     readonly autoComplete = 'off';
+    readonly excludedValues: Set<string>;
     @serializable() readonly type: string = DYNAMIC_FORM_CONTROL_TYPE_INSTITUTION_AFFILIATION_SELECT;
+
+  constructor(config: DsDynamicInstitutionAffiliationSelectModelConfig, layout?: DynamicFormControlLayout) {
+    super(config, layout);
+    this.excludedValues = new Set(config?.excludedValues || []);
+  }
 }
