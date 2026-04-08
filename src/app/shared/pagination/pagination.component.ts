3c8a83a2d6910e90d6b19e5bf9970c0fcf934b01
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2026-04-08 10:32:31 +0200

    [ADD] feat(gear): Gear to select number of search result per page.

--- src/app/shared/pagination/pagination.component.ts
+++ src/app/shared/pagination/pagination.component.ts
@@ -158,7 +158,7 @@ export class PaginationComponent implements OnChanges, OnDestroy, OnInit {
   /**
    * Option for hiding the gear
    */
-  @Input() public hideSortOptions = false;
+  @Input() public hideSortOptions = true;
 
   /**
    * Option for hiding the pager when there is less than 2 pages
